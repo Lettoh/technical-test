d6367aa8737b5f8a9553a19c2cad5603d243bd96
Frédéric LEONARD <fleonard@student.le-101.fr>
2024-03-26 13:08:14 +0100

    Fresh start with basics

---
+++ app/Http/Resources/ProfileResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'last_name' => $this->name,
+            'first_name' => $this->first_name,
+            'img' => $this->img,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
