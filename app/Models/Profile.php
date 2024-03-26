cc3e70daa576951682e49a073f7b9cfe0ba9de68
Frédéric LEONARD <fleonard@student.le-101.fr>
2024-03-26 19:11:38 +0100

    Clean profile.php

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -11,9 +11,4 @@ class Profile extends Model
 
     protected $fillable = ["last_name", 'first_name', "img", "status"];
 
-    public function add($array)
-    {
-        return parent::create($array);
-    }
-
 }
